617751042b52bb63aa3bec36e06c21b172c9c66f
Kyle Brooks <kbrooks353@gmail.com>
2019-09-24 20:01:15 -0700

    added arrays and began functions

---
+++ script.js
@@ -0,0 +1,35 @@
+// created arrays
+
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+// create an array with the other arrays in it
+
+var allArr = [lowerCase, nums];
+
+// create a function that randomizes an index in an array
+
+function randomIndex(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+
+// create a function that calls randomIndex with a random arr the number of times the user selected
+
+function generatePass() {
+    var passLength = prompt("Choose a password length between 8 and 128 characters.");
+    for (var i = 0; i < 10; i++) {
+        if (isNaN(passLength)) {
+            passLength = prompt("Enter a number between 8 and 128!");
+        } else if (passLength < 8) {
+            passLength = prompt("I said BETWEEN 8 and 128!");
+        } else if (passLength > 128) {
+            passLength = prompt("I said BETWEEN 8 and 128!");
+        }
+    }
+    for (var j = 0; j <= passLength; j++){
+        
+    }
+
+}
+
+generatePass();
